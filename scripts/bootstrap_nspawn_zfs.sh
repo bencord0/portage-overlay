496c0996b0fac86532453986f46412fb8810ceca
Ben Cordero <bencord0@condi.me>
2023-11-05 15:21:08 +0000

    scripts: New script bootstrap nspawn container on zfs

---
+++ scripts/bootstrap_nspawn_zfs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -ex
+
+if [[ -f /etc/nspawn-bootstrap.sh ]]; then
+    source /etc/nspawn-bootstrap.sh
+fi
+
+profile="${1}"
+if [[ -z "${profile}" ]]; then
+    echo "usage: ${0} <profile>"
+    exit 1
+fi
+
+machine="${machine_prefix}${profile}"
+root="/var/lib/machines/${machine}"
+zpool="${zpool:-zpool}"
+
+# Create machine dataset
+zfs create -o "mountpoint=${root}" "${zpool}/machines/${machine}"
+
+# Set initial profile
+mkdir -pv "${root}/etc/portage"
+ln -sf /var/db/repos/bencord0/profiles/default/linux/amd64/nspawn "${root}/etc/portage/make.profile"
+
+cat << EOF > "${root}/etc/portage/repos.conf"
+[gentoo]
+location = /var/db/repos/gentoo
+
+[bencord0]
+location = /var/db/repos/bencord0
+EOF
+
+# In lieu of a stage3 tarball
+emerge --jobs --root="${root}" --sysroot="${root}" --config-root="${root}" sys-apps/baselayout
+USE="build -udev" emerge --jobs --root="${root}" --sysroot="${root}" --config-root="${root}" @system
+
+# Set final profile
+rm -v "${root}/etc/portage/make.profile"
+ln -sf "/var/db/repos/bencord0/profiles/host/${profile}" "${root}/etc/portage/make.profile"
+zfs snap "${zpool}/machines/${machine}@gentoo-clean"
+
+cat << EOF > "/etc/systemd/nspawn/${machine}.nspawn"
+[Exec]
+Boot=yes
+LinkJournal=try-guest
+PrivateUsers=no
+
+[Files]
+BindReadOnly=/var/db/repos
+Bind=/var/cache/distfiles
+Bind=/var/cache/binpkgs
+
+[Network]
+Private=yes
+VirtualEthernet=yes
+Bridge=br0
+EOF
