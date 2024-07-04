198a36a7667a14122fb310e78428d533e9d4ad07
Ben Cordero <bencord0@condi.me>
2024-07-04 21:21:45 +0100

    profiles: Update amd64/nspawn into nspawn-23 with merged-usr

--- scripts/bootstrap_nspawn_zfs.sh
+++ scripts/bootstrap_nspawn_zfs.sh
@@ -20,7 +20,7 @@ zfs create -o "mountpoint=${root}" "${zpool}/machines/${machine}"
 
 # Set initial profile
 mkdir -pv "${root}/etc/portage"
-ln -sf /var/db/repos/bencord0/profiles/default/linux/amd64/nspawn "${root}/etc/portage/make.profile"
+ln -sf /var/db/repos/bencord0/profiles/default/linux/amd64/nspawn-23 "${root}/etc/portage/make.profile"
 
 cat << EOF > "${root}/etc/portage/repos.conf"
 [gentoo]
